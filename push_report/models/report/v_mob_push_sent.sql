497697e9eed4ceef92c946d6df5e132bc5691acd
Alexey Khromenko <a.khromenko@banki.ru>
2022-12-30 02:13:45 +0300

    my first commit

--- push_report/models/report/v_mob_push_sent.sql
+++ push_report/models/report/v_mob_push_sent.sql
@@ -1,14 +1,13 @@
---{{ config(materialized='table') }}
 select
 	       coalesce(mps.create_ts::date, tld.date) as create_ts,
 	       coalesce(mps.source, tld.last_src) as last_src,
 	       mps.title,
 	       mps.template_text,
 	       sum(mps.msg_sent_cnt) as msg_sent_count,
-	       max(category_2_name),
-	       max(revenue_fact),
-	       max(ho_clicks),
-	       max(ho_issued)
+	       max(category_2_name) as category_2_name,
+	       max(revenue_fact) as revenue_fact,
+	       max(ho_clicks) as ho_clicks,
+	       max(ho_issued) as ho_issued
 	from {{ ref('mob_push_msg_sent')}} mps
 	full outer join {{ ref('t_leadgen_data_light_push')}} tld
 	       on tld.last_src = mps.source
