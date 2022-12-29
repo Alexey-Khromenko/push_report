2e7c8ec6f8886789f0c265ac608821adc49b84c6
Alexey Khromenko <a.khromenko@banki.ru>
2022-12-30 01:49:47 +0300

    my first commit

---
+++ push_report/models/base/mob_push_msg_sent.sql
@@ -0,0 +1,13 @@
+select 
+	pq.create_ts,
+        pq.source,
+        pq.status_code,
+	pt.title,
+	pt.msg_text as template_text,
+	1 as msg_sent_cnt
+from sms.t_push_queue pq
+left join sms.d_push_template pt
+	on pt.code::text = pq.template_id::text
+where pq.create_ts >=date'2022-11-10'
+and pq.create_ts >=date_trunc('month',current_date) - '2 month'::interval
+and pq.status_code = 'SENT'
